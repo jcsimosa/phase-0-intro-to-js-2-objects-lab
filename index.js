ae9abdfd33cb4b00cf3bc54e709717caec34212b
Jose Simosa Nunez <josecarlossimosa@gmail.com>
2022-06-16 12:36:19 -0500

    commit tree df986e331b36258bf67f9f851b5c07fdc5de7257

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+const employee = {
+    jose : '6304 Kaiwiki Pl',
+    juan : '3844 Keanu St',
+}
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    const newObj = { ...obj };
+
+  newObj[key] = value;
+
+  return newObj;
+}
+let allNew = updateEmployeeWithKeyAndValue(employee,'juan','3855 Keanu St');
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj['name'] = 'Sam';
+    obj['streetAddress'] = '12 Broadway'
+    return obj
+  }
+  const Newcopy = destructivelyUpdateEmployeeWithKeyAndValue(employee,'juan','3855 Keanu Pl');
+
+  function deleteFromEmployeeByKey(employee, key){
+    
+    const copyTest = {...employee};
+    delete copyTest[key]
+    return copyTest
+
+  };
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+  }
